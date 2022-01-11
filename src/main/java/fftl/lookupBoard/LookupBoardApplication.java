ba5386588d5b1bbd4bd58215b6a3854698363fc7
fftl <fftl.0710@gmail.com>
2022-01-11 11:23:04 +0900

    project init

---
+++ src/main/java/fftl/lookupBoard/LookupBoardApplication.java
@@ -0,0 +1,13 @@
+package fftl.lookupBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookupBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookupBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fftl/lookupBoard/LookupBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package fftl.lookupBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookupBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
